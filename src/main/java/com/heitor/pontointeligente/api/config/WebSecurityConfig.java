ae66817c3a8d376ccd528a0bb89cc06af0a7d4a5
Heitor Teixeira <heitoret@gmail.com>
2018-11-24 16:03:01 -0200

    Controller cadastro PF

--- src/main/java/com/heitor/pontointeligente/api/config/WebSecurityConfig.java
+++ src/main/java/com/heitor/pontointeligente/api/config/WebSecurityConfig.java
@@ -20,7 +20,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
       web
         .ignoring()
-           .antMatchers("/api/cadastrar-pj/**"); 
+           .antMatchers("/api/**"); 
     }
     
 }
